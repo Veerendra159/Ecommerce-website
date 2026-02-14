188067ec38c5dac74d42d362fa3b75d775fab0fd
mogili veerendra kumar <veerendrakumarmogili@gmail.com>
2026-02-14 23:29:38 +0530

    order route is added

--- Backend/controllers/ordercontroller.js
+++ Backend/controllers/ordercontroller.js
@@ -28,3 +28,25 @@ export const addOrders = async (req, res) => {
     }
 
 }
+
+// get user order
+export const getOrder = async (req, res) => {
+    try {
+        const userID = req.user._id;
+        const findUser = await orders_model.findOne({ user: userID }).populate("products.productId")
+        if (!findUser) {
+            return res.status(400).json({
+                message: "your order are not found"
+            })
+        }
+        return res.status(200).json({
+            findUser,
+            message: "your orders"
+        })
+    } catch (error) {
+        console.log("error", error.message);
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,10 +1,12 @@
 import { protect } from "../middlewares/authmiddleware.js";
 import { admin_protect } from "../middlewares/adminMiddleware.js";
-import { addOrders } from "../controllers/ordercontroller.js";
+import { addOrders, getOrder } from "../controllers/ordercontroller.js";
 import express from "express"
+import { get } from "mongoose";
 
 const router = express();
 
 router.post("/AddOrders", protect, addOrders)
+router.get("/GetORders", protect, getOrder)
 
 export default router;
